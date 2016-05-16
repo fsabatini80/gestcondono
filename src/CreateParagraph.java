b221469c7f7e7295b9439ac1ff5bc79d66d3cdcc
fsabatini80 <f.sabatini80@gmail.com>
2016-05-16 13:12:50 +0200

    stampa lettera abuso

---
+++ src/CreateParagraph.java
@@ -0,0 +1,27 @@
+import it.soft.service.WordService;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+public class CreateParagraph {
+	public static void main(String[] args) throws Exception {
+
+		// Blank Document
+		XWPFDocument document = new XWPFDocument();
+		// Write the Document in file system
+		FileOutputStream out = new FileOutputStream(new File(
+				"createparagraph.docx"));
+
+//		WordService.createPage1(document);
+//		WordService.createPage2(document);
+//		WordService.createPage3(document);
+//		WordService.createPage4(document);
+
+		document.write(out);
+		out.close();
+		System.out.println("createparagraph.docx written successfully");
+	}
+
+}
